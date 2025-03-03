1bb4c7a25c8839a5c0a0039ef6e8514310a14c43
cpmexe29 <akinisheve@gmail.com>
2025-03-03 19:58:20 +0300

    Add files via upload

---
+++ TaskCPP/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a{5}, b{6};
+    cout << "__Input numbers__" << endl;
+
+    cout << "A + B = " << a + b << endl;
+    return 0;
+}
